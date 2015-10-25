8b6e36d1b01b6405af7f2172e3e9e59709b1b215
Андрей <andwebar@gmail.com>
2015-10-25 13:47:19 +0500

    update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,25 @@
 src=`pwd`
 dest="${dest:-~/builds/dotfiles-dest}"
 
+folders=(
+  ".config/i3"
+  ".config/i3status"
+  ".config/mpd"
+  ".config/ncmpcpp"
+  ".config/themer"
+)
+
 echo "Source directory is: $src"
 echo "Destination directory is: $dest"
 
+for f in "${folders[@]}"
+do
+    mkdir -p "$dest"/$f
+done
+
 dotrix --src $src --dst $dest $@
+
 themer render all
-ln -s ~/.config/themer/current/i3.conf ~/.config/i3/config
+themer activate rain
+
+ln -s "$dest"/.config/themer/current/i3.conf "$dest"/.config/i3/config
