29d245c31a0c0e2946056f948c73368181aa10f4
Andrew <goodmind@users.noreply.github.com>
2015-10-25 13:00:31 +0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,5 @@ echo "Source directory is: $src"
 echo "Destination directory is: $dest"
 
 dotrix --src $src --dst $dest $@
+themer render all
+ln -s ~/.config/themer/current/i3.conf ~/.config/i3/config
